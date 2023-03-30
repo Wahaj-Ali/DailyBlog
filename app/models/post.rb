26ea03913633717d2e7d0856284fd1c7edb5daa8
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-30 18:27:50 +0500

    Updated models with all required methods.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,15 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+  after_save :update_author_posts_counter
 
-  def comments_counter
-    comments.count
+  def get_recent_comments(count = 5)
+    comments.order(created_at: :desc).limit(count)
   end
 
-  def likes_counter
-    likes.count
+  private
+
+  def update_author_posts_counter
+    author.update(posts_counter: Post.where(author_id).count)
   end
 end
